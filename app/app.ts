c292f42ceabde623e0a41683a3fc04f320112709
flolu <loflude@gmail.com>
2023-02-20 19:29:23 +0100

    🩹 workaround for inferred type error

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,14 @@
 import {number} from '@org/lib'
 import * as express from 'express'
 
-const app = express()
+/**
+ * Workaround to infer type
+ * https://github.com/aspect-build/rules_ts/issues/159
+ */
+const app: ReturnType<typeof express> = express()
 
 app.get('/', (_req, res) => {
   res.send({number})
 })
 
-/**
- * TODO The inferred type of 'app' cannot be named without a reference
- * to @types/express-serve-static-core
- *
- * https://github.com/aspect-build/rules_ts/issues/159
- * https://github.com/microsoft/TypeScript/issues/42873
- */
-export default app as any
+export default app
